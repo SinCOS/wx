b56972fbdb64ac2fe50c0aa88003e47cf5059412
qyf1994 <qyf1994@gmail.com>
2018-05-18 13:50:14 +0800

    first commit

---
+++ config/wxxcx.php
@@ -0,0 +1,7 @@
+<?php 
+
+	return [
+		'appid' => 'wx15c6e32f00dc1860',
+		'secret' = 'aa47b4cbb2e5172d741604792b4f97f1',
+		'code2session_url' => "https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code",
+	];
